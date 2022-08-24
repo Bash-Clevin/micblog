8bf865531c02ac815c82d4638b0d5134ae4ee23b
kevin otieno <kevin.otieno@megatherium.solutions>
2022-08-24 08:57:08 +0300

    feat: finish up micro services

--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -9,14 +10,8 @@ app.use(cors())
 const port = 4002
 const posts = {}
 
-app.get('/posts', (req, res) => {
-    res.send(posts)
-})
-
-app.post('/events', (req,res) =>{
-    const { type, data } = req.body;
-
-    if(type === 'PostCreated') {
+const handleEvent = (type,data) => {
+    if (type === 'PostCreated') {
         const { id,title } = data
 
         posts[id] = { id,title,comments: [] }
@@ -40,10 +35,29 @@ app.post('/events', (req,res) =>{
         comment.status = status
         comment.content = content
     }
+}
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
+app.post('/events', (req,res) =>{
+    const { type, data } = req.body;
+    
+    handleEvent(type,data)
 
     res.send({})
 })
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`Query listening on port ${port}`)
+
+    // get events that  were missed during downtime
+    const res = await axios.get('http://localhost:4005/events')
+
+    for (let event of res.data) {
+        console.log('Processing event', event.type)
+
+        handleEvent(event.type, event.data)
+    }
 });
