a0004c590b82b00c894e39d5bf6c01ce9e1cf15d
kevin otieno <kevin.otieno@megatherium.solutions>
2022-08-24 08:10:53 +0300

    feat: add moderation service

--- event_bus/index.js
+++ event_bus/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser')
 const axios = require('axios')
 
 const app = express()
-app.use(bodyParser.json)
+app.use(bodyParser.json())
 
 const port = 4005
 
@@ -13,6 +13,7 @@ app.post('/events', (req, res) => {
     axios.post('http://localhost:4000/events', event)
     axios.post('http://localhost:4001/events', event)
     axios.post('http://localhost:4002/events', event)
+    axios.post('http://localhost:4003/events', event)
 
     res.send({ status: 'OK' })
 })
--- query/index.js
+++ query/index.js
@@ -7,10 +7,10 @@ app.use(bodyParser.json())
 app.use(cors())
 
 const port = 4002
-const post = {}
+const posts = {}
 
-app.get('/post', (req, res) => {
-    res.send(post)
+app.get('/posts', (req, res) => {
+    res.send(posts)
 })
 
 app.post('/events', (req,res) =>{
@@ -23,15 +23,27 @@ app.post('/events', (req,res) =>{
     }
 
     if (type === 'CommentCreated') {
-        const { id, content, postId } = data
+        const { id, content, postId, status } = data
         
         const post = posts[postId]
-        post.comments.push({ id, content })
+        post.comments.push({ id, content, status })
+    }
+
+    if (type === 'CommentUpdated') {
+        const { id, content, postId, status } = data
+
+        const post = posts[postId]
+        const comment = post.comments.find(comment => {
+            return comment.id == id
+        })
+
+        comment.status = status
+        comment.content = content
     }
 
     res.send({})
 })
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`)
+    console.log(`Query listening on port ${port}`)
 });
